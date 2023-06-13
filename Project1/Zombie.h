5a617c974cc7c912f6b3d37ba4fe028368012625
FrowsyFrog <frowsyfrog@gmail.com>
2023-06-13 08:10:20 -0500

    add zombies

--- Project1/Zombie.h
+++ Project1/Zombie.h
@@ -2,5 +2,13 @@
 #include "Agent.h"
 class Zombie : public Agent
 {
+protected:
+	glm::vec2 direction;
+public:
+	Zombie();
+	~Zombie();
+	void init(float speed, glm::vec2 position);
+	void update(const vector<string>& levelData, vector<Human*>& humans,
+		vector<Zombie*>& zombies);
 };
 
